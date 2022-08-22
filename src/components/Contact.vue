c7d388c7b1ad85dc11b189e98a228ee8b9ca6bd1
rmercado6 <ricardo.97.mc@gmail.com>
2022-08-22 13:01:55 -0500

    feat: add contact panel

---
+++ src/components/Contact.vue
@@ -0,0 +1,31 @@
+<script setup>
+</script>
+
+<template>
+  <div class="h-screen flex flex-col justify-center items-center">
+    <div class="w-full">
+
+      <div class="flex flex-col items-center p-4 sm:p-8">
+        <div class="mb-4 sm:mb-8">
+          <h2 class="text-xl sm:text-2xl lg:text-3xl font-bold text-center">Contacto</h2>
+        </div>
+
+        <div class="grid grid-cols-2 gap-5 w-full">
+          <span class="text-end">email:</span>
+          <span>
+            <a href="mailto:contacto@rmercado.dev">contacto@rmercado.dev</a>
+          </span>
+          <span class="text-end">linked-in:</span>
+          <span>
+            <a href="#" target="_blank">rmercado</a>
+          </span>
+          <span class="text-end">github:</span>
+          <span>
+            <a href="https://github.com/rmercado6" target="_blank">rmercado6</a>
+          </span>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@ import TheWelcome from '@/components/TheWelcome.vue'
 import Timeline from '@/components/Timeline.vue'
 import Education from '@/components/Education.vue'
 import Skills from '@/components/Skills.vue'
+import Contact from '@/components/Contact.vue'
 </script>
 
 <template>
@@ -11,5 +12,6 @@ import Skills from '@/components/Skills.vue'
     <Timeline />
     <Education />
     <Skills />
+    <Contact />
   </main>
 </template>
