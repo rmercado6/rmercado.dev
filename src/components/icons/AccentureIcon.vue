f156f6b58643bf765d64ec98761a9e82614f2318
Ricardo Mercado <rmercadoc@Mirror-Shield.local>
2023-09-21 01:51:10 +0100

    feat: update work experience

--- src/components/icons/AccentureIcon.vue
+++ src/components/icons/AccentureIcon.vue
@@ -1,6 +1,6 @@
 <!-- This icon is from <https://github.com/Templarian/MaterialDesign>, distributed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0) license-->
 <template>
   <img 
-    src="/axtel.png"
+    src="/accenture.png"
   />
 </template>
