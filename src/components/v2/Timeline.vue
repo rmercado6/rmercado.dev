5be902bef6b6eaf1c8e53b7349b1c0cba38383dd
Ricardo Mercado <ricardo.97.mc@gmail.com>
2025-06-28 00:17:29 +0100

    feat(Timeline): refactor timeline items itno ts file and restyle light mode

--- src/components/v2/Timeline.vue
+++ src/components/v2/Timeline.vue
@@ -1,88 +1,21 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue'
-import { TimelineItem } from '@/types/timeline'
+import { timelineItems, TimelineItem } from '@/types/timeline'
 
 const selectedCategory = ref('all')
-
-const timelineItems: TimelineItem[] = [
-  new TimelineItem(
-    'MSc Advanced Technologies For Financial Computing',
-    'University of Edinburgh',
-    {
-      start: { day: 11, month: 9, year: 2023 },
-      end: { day: 21, month: 11, year: 2024 }
-    },
-    'studies',
-    '/accenture.png'
-  ),
-  new TimelineItem(
-    'BSc Software Engineering',
-    'Universidad Autónoma de Nuevo León',
-    {
-      start: { day: 1, month: 8, year: 2015 },
-      end: { day: 31, month: 5, year: 2020 }
-    },
-    'studies'
-  ),
-  new TimelineItem(
-    'Internation Baccaleureate Organization Diploma Progranmme',
-    'Universidad de Monterrey',
-    {
-      start: { day: 1, month: 8, year: 2013 },
-      end: { day: 30, month: 6, year: 2015 }
-    },
-    'studies'
-  ),
-  new TimelineItem(
-    'Professional Experience',
-    'Professional Experience',
-    {
-      start: { day: 1, month: 1, year: 2020 },
-      end: { day: 1, month: 1, year: 2024 }
-    },
-    'professional'
-  ),
-  new TimelineItem(
-    'Professional Experience',
-    'Professional Experience',
-    {
-      start: { day: 1, month: 1, year: 2020 },
-      end: { day: 1, month: 1, year: 2024 }
-    },
-    'professional'
-  ),
-  new TimelineItem(
-    'Personal Projects',
-    'Personal Projects',
-    {
-      start: { day: 1, month: 1, year: 2020 },
-      end: { day: 1, month: 1, year: 2024 }
-    },
-    'personal'
-  ),
-  new TimelineItem(
-    'Music',
-    'Music',
-    {
-      start: { day: 1, month: 1, year: 2020 },
-      end: { day: 1, month: 1, year: 2024 }
-    },
-    'music'
-  )
-]
 
 const filteredTimelineItems = computed(() => {
   if (selectedCategory.value === 'all') {
     return timelineItems
   }
   return timelineItems.filter(
-    (item) => item.category === selectedCategory.value
+    (item: TimelineItem) => item.category === selectedCategory.value
   )
 })
 
 function getFilteredTimelineItemsByYear(year: number) {
   return filteredTimelineItems.value.filter(
-    (item) => item.date.end.year === year
+    (item: TimelineItem) => item.date.end.year === year
   )
 }
 
@@ -93,42 +26,33 @@ function getYears() {
   }
   return Array.from(years).sort((a, b) => b - a)
 }
+
+const categories = [
+  { name: 'All', key: 'all' },
+  { name: 'Studies', key: 'studies' },
+  { name: 'Professional Experience', key: 'professional' },
+  { name: 'Personal Projects', key: 'personal' },
+  { name: 'Music', key: 'music' }
+]
 </script>
+
 <template>
   <div
     class="flex flex-col items-center justify-start min-h-screen my-10 md:my-0"
   >
     <div class="flex justify-center gap-4 pb-6">
-      <a
-        @click="selectedCategory = 'all'"
-        @mouseenter="selectedCategory = 'all'"
-      >
-        All
-      </a>
-      <a
-        @click="selectedCategory = 'studies'"
-        @mouseenter="selectedCategory = 'studies'"
-      >
-        Studies</a
-      >
-      <a
-        @click="selectedCategory = 'professional'"
-        @mouseenter="selectedCategory = 'professional'"
-      >
-        Professional Experience
-      </a>
-      <a
-        @click="selectedCategory = 'personal'"
-        @mouseenter="selectedCategory = 'personal'"
-      >
-        Personal Projects
-      </a>
-      <a
-        @click="selectedCategory = 'music'"
-        @mouseenter="selectedCategory = 'music'"
-      >
-        Music
-      </a>
+      <template v-for="category in categories" :key="category.key">
+        <a
+          @click="selectedCategory = category.key"
+          @mouseenter="selectedCategory = category.key"
+          :class="{
+            'underline font-bold px-2 bg-gray-400/20':
+              selectedCategory === category.key
+          }"
+        >
+          {{ category.name }}
+        </a>
+      </template>
     </div>
     <div class="flex flex-col items-center timeline">
       <span
@@ -141,12 +65,14 @@ function getYears() {
           v-for="item in getFilteredTimelineItemsByYear(year)"
           :key="item.title"
         >
-          <div class="flex p-2 border border-gray-200 timelineItem gap-3">
+          <div
+            class="flex p-2 border dark:border-gray-200 border-gray-400 timelineItem gap-3"
+          >
             <div
               v-if="item.img"
-              class="aspect-square max-h-full border border-gray-200/30"
+              class="aspect-square max-h-full border dark:border-gray-200/30 border-gray-400"
             >
-              <img src="/accenture.png" class="max-h-full h-12" />
+              <img :src="item.img" class="max-h-full h-12" alt="event image" />
             </div>
             <div class="flex flex-col">
               <span>{{ item.title }}</span>
@@ -160,6 +86,7 @@ function getYears() {
     </div>
   </div>
 </template>
+
 <style scoped>
 a {
   cursor: pointer;
@@ -177,6 +104,7 @@ div.timelineItem {
   height: 20px;
   border-left: 1px solid;
   position: absolute;
+  @apply dark:border-gray-200 border-gray-400;
 }
 
 :after {
--- src/types/timeline.ts
+++ src/types/timeline.ts
@@ -46,3 +46,70 @@ export class TimelineItem {
     return new Date(this.date.end.year, this.date.end.month, this.date.end.day)
   }
 }
+
+export const timelineItems: TimelineItem[] = [
+  new TimelineItem(
+    'MSc Advanced Technologies For Financial Computing',
+    'University of Edinburgh',
+    {
+      start: { day: 11, month: 9, year: 2023 },
+      end: { day: 21, month: 11, year: 2024 }
+    },
+    'studies',
+    '/accenture.png'
+  ),
+  new TimelineItem(
+    'BSc Software Engineering',
+    'Universidad Autónoma de Nuevo León',
+    {
+      start: { day: 1, month: 8, year: 2015 },
+      end: { day: 31, month: 5, year: 2020 }
+    },
+    'studies'
+  ),
+  new TimelineItem(
+    'Internation Baccaleureate Organization Diploma Progranmme',
+    'Universidad de Monterrey',
+    {
+      start: { day: 1, month: 8, year: 2013 },
+      end: { day: 30, month: 6, year: 2015 }
+    },
+    'studies'
+  ),
+  new TimelineItem(
+    'Professional Experience',
+    'Professional Experience',
+    {
+      start: { day: 1, month: 1, year: 2020 },
+      end: { day: 1, month: 1, year: 2024 }
+    },
+    'professional'
+  ),
+  new TimelineItem(
+    'Professional Experience',
+    'Professional Experience',
+    {
+      start: { day: 1, month: 1, year: 2020 },
+      end: { day: 1, month: 1, year: 2024 }
+    },
+    'professional'
+  ),
+  new TimelineItem(
+    'Personal Projects',
+    'Personal Projects',
+    {
+      start: { day: 1, month: 1, year: 2020 },
+      end: { day: 1, month: 1, year: 2024 }
+    },
+    'personal'
+  ),
+  new TimelineItem(
+    'Music',
+    'Music',
+    {
+      start: { day: 1, month: 1, year: 2020 },
+      end: { day: 1, month: 1, year: 2024 }
+    },
+    'music'
+  )
+]
